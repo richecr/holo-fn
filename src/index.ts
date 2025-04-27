46c6c2c5a8936b2bacd1894371f31a6bce4e49ca
Rich Ramalho <richecr1799@gmail.com>
2025-04-26 22:25:34 -0300

    Deployed e8c6906 with MkDocs version: 1.6.1

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './maybe';
-export * from './either';
-export * from './result';
-
-export { Just, Nothing, fromNullable } from './maybe';
-export { Right, Left, tryCatch, fromPromise, fromAsync } from './either';
-export { Ok, Err, fromThrowable, fromPromise as fromPromiseResult, fromAsync as fromAsyncResult } from './result';
--- src/maybe/index.ts
+++
@@ -1,9 +0,0 @@
-export { Maybe } from "./Maybe";
-export { Just } from "./Maybe";
-export { Nothing } from "./Maybe";
-export { fromNullable } from "./Maybe";
-export { mapM } from "./Maybe";
-export { chainM } from "./Maybe";
-export { unwrapOrM } from "./Maybe";
-export { matchM } from "./Maybe";
-export { equalsM } from "./Maybe";
